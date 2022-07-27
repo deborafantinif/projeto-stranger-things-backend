d2074202b05a22d4cfe9b5b00d37774fd0c9e8a8
deborafantinif <deboraferfan@gmail.com>
2022-07-27 14:09:18 -0300

    fix: definindo a UPSIDEDOWN de acordo com as variaveis de ambiente

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
